e27e7f82d300bb17512d8a22c0a30412e41772c7
guadarecio <guadaluperecio@gmail.com>
2022-10-19 17:06:04 +0200

    Making the folders and creating screens

---
+++ src/screens/AddSubmission.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const AddSubmission = () => {
+  return (
+    <View>
+      <Text>AddSubmission</Text>
+    </View>
+  );
+};
+
+export default AddSubmission;
---
+++ src/screens/SubmissionsList.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const SubmissionsList = () => {
+  return (
+    <View>
+      <Text>SubmissionsList</Text>
+    </View>
+  );
+};
+
+export default SubmissionsList;
